d30d89d7ddd6ecbc2c9fd341b48cec2e461d6b85
Diego Tejada <stanleytejadaj@gmail.com>
2024-05-11 19:52:13 -0700

    Solving all the bugs just left one: Expected time to end with '(next day)' when it is the next day.

--- computer sciencie/projects/time_calculator.py
+++ computer sciencie/projects/time_calculator.py
@@ -1,23 +1,27 @@
 def add_time(start, duration, day=''):
-
+    #splitting time duration 
     time, afte_post = start.split(' ')
-    hour, minute = time.split(':')
-    days = ["monday", 'tuesday', 'wednesday', 'thrusday', 'friday', 'saturday','sunday']
-    meridiam = ['AM', 'PM']
+    hour, minute = time.split(':') 
+    days = ["monday", 'tuesday', 'wednesday', 'thrusday', 'friday', 'saturday','sunday'] #list of days
+    meridiam = ['AM', 'PM'] #day or night
     dhour, dminute = duration.split(':')
 
-    days_later = int(dhour)/24+1
+    if int(dhour) > 0 and int(dhour)<=24:
+        days_later = int(dhour)/24 #calculating days later
+    else:
+        days_later = int(dhour)/24+1 #calculating days later
     
     clock = 12
-    change=0
     days_count = 0
     
-    total_minutes = (int(hour) * 60) + (int(dhour) * 60) + int(minute) + int(dminute)
-    new_hour = total_minutes // 60
-    new_minutes = total_minutes % 60
+    total_minutes = (int(hour) * 60) + (int(dhour) * 60) + int(minute) + int(dminute) #passing whole in minutes
+    new_hour = total_minutes // 60 #calculating hours
+    new_minutes = total_minutes % 60 #calculating minutes
     
-    if afte_post == meridiam[1]:
-        meridiam[0], meridiam[1] = meridiam[1], meridiam[0]
+    auxiliary = 0
+    
+    if afte_post == meridiam[1]: 
+        meridiam[0], meridiam[1] = meridiam[1], meridiam[0] #change a,b = b,a
     
     for turn in range((new_hour//clock)):
         if new_hour > clock:
@@ -28,11 +32,13 @@ def add_time(start, duration, day=''):
         elif new_hour == 12:
                 meridiam[0], meridiam[1] = meridiam[1], meridiam[0]
                 days_count += 0.5
+                days_later+=1
 
         if new_minutes > 60:
             new_minutes -=60
             new_hour += 1
-
+            
+        auxiliary += 1
             
     if len(str(new_minutes)) == 1:
         new_minutes = '0'+ str(new_minutes)
@@ -42,26 +48,42 @@ def add_time(start, duration, day=''):
             days_count += d
     
     if day!='':
-        if int(days_count) == 1:
+        print(days_later)
+        if days_count > 1 and afte_post != meridiam[0]:
+            days_count +=0.5
+        if int(days_count) < 1:
+            formatted = str(new_hour) + ':' + str(new_minutes) + ' ' + meridiam[0] + ', ' + days[int(days_count)].capitalize()
+            return formatted
+        elif int(dhour) == 24 and int(dminute)==0:
+            formatted = str(new_hour) + ':' + str(new_minutes) + ' ' + meridiam[0] + ', ' + days[int(days_count)].capitalize() + ' (next day)'
+        elif int(days_count) == 1:
             formatted = str(new_hour) + ':' + str(new_minutes) + ' ' + meridiam[0] + ', ' + days[int(days_count)].capitalize() + ' (next day)'
             return formatted
         elif int(days_count) > 1:
             days_count=days_count % 7
             if days[int(days_count)] != days[6] and days_count<6:
                 days_count += 1
+            
             else: 
                 days_count = 0
-            formatted = str(new_hour) + ':' + str(new_minutes) + ' ' + meridiam[0] + ', ' + days[int(days_count)].capitalize() + f' ({int(days_later)} days later)'
+            formatted = str(new_hour) + ':' + str(new_minutes) + ' ' + meridiam[0] + ', ' + days[int(days_count)-1].capitalize() + f' ({int(days_later)} days later)'
             return formatted
         else:
             formatted = str(new_hour) + ':' + str(new_minutes) + ' ' + meridiam[0] + ', ' + days[int(days_count)].capitalize()
             return formatted
     elif day=='':
-        print(days_count)
+        if days_count > 1 and afte_post != meridiam[0]:
+            days_count +=0.5
+
+        if int(days_count) < 1:
+            formatted = str(new_hour) + ':' + str(new_minutes) + ' ' + meridiam[0]
+            return formatted
+        
         if days_count == 1:
             formatted = str(new_hour) + ':' + str(new_minutes) + ' ' + meridiam[0]  + ' (next day)'
             return formatted
         elif int(days_count) > 1:
+            
             formatted = str(new_hour) + ':' + str(new_minutes) + ' ' + meridiam[0] + f' ({int(days_count)} days later)'
             return formatted
         else:
@@ -72,3 +94,4 @@ def add_time(start, duration, day=''):
     return  formatted
 
 print(add_time('2:59 AM', '24:00', 'saturDay'))
+print('2:59 AM, Sunday (next day)')
